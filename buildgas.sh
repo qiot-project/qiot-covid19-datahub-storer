aa98fed992b3d1ab21caa733421aa973febf37af
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-03-28 19:26:46 +0200

    prepare for RC1

--- buildgas.sh
+++
@@ -1,8 +0,0 @@
-chmod +x mvn*
-mvn -N io.takari:maven:wrapper
-./mvnw clean package -Pnative -pl qiot-datahub-storer-gas -am -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiotcovid19/qiot-datahub-storer-gas:2.0.0-alpha
-cd qiot-datahub-storer-gas
-docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-storer-gas:2.0.0-alpha .
-cd ..
-docker push quay.io/qiotcovid19/qiot-datahub-storer-gas:2.0.0-alpha
--- buildpollution.jvm.sh
+++ buildpollution.jvm.sh
@@ -2,8 +2,8 @@ chmod +x mvn*
 mvn -N io.takari:maven:wrapper
 ./mvnw clean package -pl qiot-datahub-storer-pollution -am -Dquarkus.package.type=fast-jar
 # -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-alpha
+docker rmi quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-RC
 cd qiot-datahub-storer-pollution
-docker build -f src/main/docker/Dockerfile.fast-jar -t quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-alpha .
+docker build -f src/main/docker/Dockerfile.fast-jar -t quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-RC .
 cd ..
-docker push quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-alpha
+docker push quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-RC

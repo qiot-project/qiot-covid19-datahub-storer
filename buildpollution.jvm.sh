909d54d40057452a9a4d828e0140ac618b947b04
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-04-17 19:13:17 +0200

    Prepare 2.0.0

--- buildpollution.jvm.sh
+++ buildpollution.jvm.sh
@@ -2,8 +2,8 @@ chmod +x mvn*
 mvn -N io.takari:maven:wrapper
 ./mvnw clean package -pl qiot-datahub-storer-pollution -am -Dquarkus.package.type=fast-jar
 # -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-RC
+docker rmi quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0
 cd qiot-datahub-storer-pollution
-docker build -f src/main/docker/Dockerfile.fast-jar -t quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-RC .
+docker build -f src/main/docker/Dockerfile.fast-jar -t quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0 .
 cd ..
-docker push quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0-RC
+docker push quay.io/qiotcovid19/qiot-datahub-storer-pollution:2.0.0
